39c1876255949bc03453909e4cafc4bd47a0806c
priss <denis.priss@disy.net>
2021-05-26 15:15:23 +0200

    Reduce turf-jsts and move into turf-buffer dir
    - Remove turf-jsts dependency
    - Add reduced set of the turf-jsts lib

---
+++ packages/turf-buffer/lib/Math.js
@@ -0,0 +1,8 @@
+/**
+ * Polyfill for IE support
+ */
+Math.trunc =
+  Math.trunc ||
+  function (x) {
+    return x < 0 ? Math.ceil(x) : Math.floor(x);
+  };
---
+++ packages/turf-buffer/lib/bufferOp.js
@@ -0,0 +1,13 @@
+/**
+ * Polyfill for IE support
+ */
+import "./Array";
+import "./Number";
+import "./Math";
+
+/**
+ * Turf JSTS dependant modules
+ *
+ * BufferOp => @turf/buffer
+ */
+export { bufferOp } from "./org/locationtech/jts/operation/buffer";
